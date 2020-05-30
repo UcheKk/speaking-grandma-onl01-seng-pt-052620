cbf26aa6489235b1a735ea6c95bdad6fc82a1d18
Uche Nweke Jr <unwekejr@gmail.com>
2020-05-30 03:23:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,7 @@ if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase
-  return "NO, NOT SINCE 1938!"
+
 end
   
 # Whatever you say to grandma, she should respond with
